37159f5a316f3d5a3c016540017576055ad06d5a
DEVINITION\tam.tran <tam.tran@vn.devinition.com>
2021-05-18 16:44:12 +0700

    login Form

--- src/components/login/loginForm.tsx
+++ src/components/login/loginForm.tsx
@@ -1,12 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { Modal } from 'react-bootstrap';
 import {Button, Form, FormGroup, Label, Input } from 'reactstrap';
-
+import "../../styles/style.scss"; 
+import '../../styles/loginForm.scss';
+import axios from 'axios';
 const loginForm = () => {
-    const [openModelLogin, setOpenModelLogin] = useState(false);
+    const [openModelLogin, setOpenModelLogin] = useState(true);
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleClose = () => setOpenModelLogin(false);
     const handleShow = () => setOpenModelLogin(true);
+
+    const submitForm = async (event: FormEvent) => {
+        event.preventDefault();
+        const info = {
+            user_name: userName,
+            password: password,
+        };
+        try {
+            await axios.post('http://localhost:5000/login', info).then(res => console.log(res)).catch(err => console.log(err));
+        } catch (err) {
+            
+        }
+    }
 
     return (
         <>
@@ -15,15 +32,16 @@ const loginForm = () => {
                 onHide={handleClose}
                 backdrop="static"
                 keyboard={false}
+                className="modal-login"
             >
                 <Modal.Header closeButton>
                     <Modal.Title>Member Login</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                <Form >
-                    <FormGroup><i className="fa fa-user"></i><Input type="text" class="form-control" placeholder="Username" required={true}></Input></FormGroup>
-                    <FormGroup><i className="fa fa-lock"></i><Input type="password" class="form-control" placeholder="Password" required={true}></Input></FormGroup>
-                    <FormGroup><Input type="submit"class="btn btn-primary btn-block btn-lg" value="Login"></Input></FormGroup> 
+                <Form onSubmit={(e) => submitForm(e)}>
+                    <FormGroup><i className="fa fa-user"></i><Input type="text"  placeholder="Username" required={true} value={userName} onChange={(e) => setUserName(e.target.value)}></Input></FormGroup>
+                    <FormGroup><i className="fa fa-lock"></i><Input type="password" placeholder="Password" required={true} value={password} onChange={(e) => setPassword(e.target.value)}></Input></FormGroup>
+                    <FormGroup><Button type="submit"className="btn btn-primary btn-block btn-lg">Login</Button></FormGroup> 
                 </Form>
                 </Modal.Body>
                 <Modal.Footer>
--- src/routers/user/Router.tsx
+++ src/routers/user/Router.tsx
@@ -6,12 +6,16 @@ import {
 } from "react-router-dom";
 import Index from '../../components/users/index';
 import AddUser from '../../components/users/AddUser';
+import LoginForm from '../../components/login/loginForm';
 
 const router = () => {
     return (
         <>
             <BrowserRouter>
                 <Switch>
+                    <Route path="/login">
+                        <LoginForm />
+                    </Route>
                     <Route path="/users/addUser">
                         <AddUser />
                     </Route>
